ac5b181bc84ea02a7e43254f54b36f3d3ee3ac70
ABunderson <76891486+ABunderson@users.noreply.github.com>
2023-05-02 15:08:10 -0600

    w03 activity

---
+++ src/js/productDetails.mjs
@@ -0,0 +1,32 @@
+import { setLocalStorage, getLocalStorage} from './utils.mjs';
+import { findProductById } from './productData.mjs';
+
+let product = {};
+
+export default async function productDetails(productId) {
+    // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    product = await findProductById(productId);
+    // once we have the product details we can render out the HTML
+    renderProductDetails();
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    document.getElementById("addToCart").addEventListener("click", addToCart);
+}
+
+function addToCart() {
+    setLocalStorage("so-cart", product);
+}
+
+function renderProductDetails() {
+    document.querySelector("#productName").innerText = product.Brand.Name;
+    document.querySelector("#productNameWithoutBrand").innerText =
+      product.NameWithoutBrand;
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").alt = product.Name;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText =
+      product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+      product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
+  }
+
