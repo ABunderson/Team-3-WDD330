b17e5d9bad7d619abaa5e24bbf22e649796d7b0a
TiannaDeSapin <u663140950@byui.edu>
2023-05-17 10:18:08 -0700

    customizable alert added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import productList from './productList.mjs';
 import { loadHeaderFooter } from './utils.mjs';
-
+import Alert from './alert.js';
 const selector = document.querySelector('.product-list');
-
+loadHeaderFooter();
 productList('tents', selector);
 
-loadHeaderFooter();
+const alertInstance = new Alert();
+alertInstance.fetchAlertsData().then(() => alertInstance.createAlertElements());
