752a174e16b545d478321c130fe658fac5b21dd3
Adrienne Bunderson <bun18015@byui.edu>
2023-05-31 12:03:57 -0600

    add sort functionality to product list

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -4,3 +4,10 @@ import { getParam, loadHeaderFooter } from './utils.mjs';
 loadHeaderFooter();
 const category = getParam('category');
 productList('.product-list', category);
+
+document.getElementById('priceSort').addEventListener('click', () => {
+  productList('.product-list', category, 'price');
+});
+document.getElementById('nameSort').addEventListener('click', () => {
+  productList('.product-list', category, 'name');
+});
--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -2,7 +2,7 @@ import { getProductsByCategory } from './externalServices.mjs';
 import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
+  return `<li class="product-card">
     <a href="/product_pages/index.html?product=${product.Id}">
     <img
       src="${product.Images.PrimaryMedium}"
@@ -12,17 +12,50 @@ function productCardTemplate(product) {
     <h2 class="card__name">${product.Name}</h2>
     <p class="product-card__price">
       <span class="product-card__discount">
-      -<span id="discount">${100 - Math.round(product.FinalPrice / product.SuggestedRetailPrice * 100)}</span>%</span>
+      -<span class="discount">${100 - Math.round(product.FinalPrice / product.SuggestedRetailPrice * 100)}</span>%</span>
        $${product.ListPrice} 
       <span class="product-card__original-price">List: $${product.SuggestedRetailPrice}</span></p></a>
   </li>`
-}     
+}
 
-export default async function productList(selector, category) {
+export default async function productList(selector, category, sortType) {
   // get the element we will insert the list into from the selector
   const el = document.querySelector(selector);
   // get the list of products
   const products = await getProductsByCategory(category);
+
+  // check for sortType and if there is one sort the products
+  if (sortType == 'price') {
+
+    // sort by price
+    products.sort((p1, p2) => (p1.FinalPrice > p2.FinalPrice) ? 1 : (p1.FinalPrice < p2.FinalPrice) ? -1 : 0);
+
+    // get the name of the current first card on the page
+    let firstCardName = document.getElementsByClassName('card__name')[0]
+
+    //compare it to the sorted first card
+    if (firstCardName.innerHTML === products[0].Name) {
+
+      // if it matches sort the other direction
+      products.sort((p1, p2) => (p1.FinalPrice < p2.FinalPrice) ? 1 : (p1.FinalPrice > p2.FinalPrice) ? -1 : 0);
+    }
+
+  } else if (sortType == 'name') {
+
+    //sort by name 
+    products.sort((p1, p2) => (p1.Name > p2.Name) ? 1 : (p1.Name < p2.Name) ? -1 : 0);
+
+    // get the name of the current first card on the page
+    let firstCardName = document.getElementsByClassName('card__name')[0]
+
+    //compare it to the sorted first card
+    if (firstCardName.innerHTML === products[0].Name) {
+
+      // if it matches sort the other direction
+      products.sort((p1, p2) => (p1.Name < p2.Name) ? 1 : (p1.Name > p2.Name) ? -1 : 0);
+    }
+  }
+  
   // render out the product list to the element
   renderListWithTemplate(productCardTemplate, el, products);
   document.querySelector(".title").innerHTML = category;
