b306e46dcb02b8ebc5bf17bd0158e7165ae016f6
Adrienne Bunderson <bun18015@byui.edu>
2023-06-09 20:05:58 -0600

    add product search

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,13 +1,32 @@
-import productList from './productList.mjs';
+import { productList, searchProducts } from './productList.mjs';
 import { getParam, loadHeaderFooter } from './utils.mjs';
 
 loadHeaderFooter();
-const category = getParam('category');
-productList('.product-list', category);
 
-document.getElementById('priceSort').addEventListener('click', () => {
-  productList('.product-list', category, 'price');
-});
-document.getElementById('nameSort').addEventListener('click', () => {
-  productList('.product-list', category, 'name');
-});
+const category = getParam('category');
+const search = getParam('search');
+
+if (category) {
+  productList('.product-list', category);
+
+  document.getElementById('priceSort').addEventListener('click', () => {
+    productList('.product-list', category, 'price');
+  });
+  document.getElementById('nameSort').addEventListener('click', () => {
+    productList('.product-list', category, 'name');
+  });
+}
+
+//go to home page if search is pushed without a value
+if (search.length === 0) {
+  window.location.href = '/index.html';
+} else if (search) {
+  searchProducts(search, '.product-list');
+
+  document.getElementById('priceSort').addEventListener('click', () => {
+    searchProducts(search, '.product-list', 'price');
+  });
+  document.getElementById('nameSort').addEventListener('click', () => {
+    searchProducts(search, '.product-list', 'name');
+  });
+}
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,73 +1,63 @@
-import { setLocalStorage, getLocalStorage, updateCartItemCount, alertMessage} from './utils.mjs';
+import { setLocalStorage, getLocalStorage, updateCartItemCount, alertMessage, clearHTMLWithMessage } from './utils.mjs';
 import { findProductById } from './externalServices.mjs';
 
 let product = {};
 
 export default async function productDetails(productId) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-  try{
-  product = await findProductById(productId);
-  // once we have the product details we can render out the HTML
-  // check to make sure the product actually exists
-  if(product) {
-    console.log(product);
-    renderProductDetails();
+  try {
+    product = await findProductById(productId);
+    // once we have the product details we can render out the HTML
+    // check to make sure the product actually exists
+    if (product) {
+      console.log(product);
+      renderProductDetails();
 
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    document.getElementById("addToCart").addEventListener("click", addToCart);
+      // once the HTML is rendered we can add a listener to Add to Cart button
+      document.getElementById("addToCart").addEventListener("click", addToCart);
 
-  } else {
-    // if the product does not exist do this
-    let htmlEl = document.querySelector(".product-detail");
-    // clear the html where the product would be
-    htmlEl.innerHTML = "";
-    //add an error message
-    const message = `<h3>Uh oh! The selected item doesn't exist.</h3>`;
-    htmlEl.innerHTML = message;
-  }
-  } catch(e) {
+    } else {
+      // if the product does not exist do this
+      clearHTMLWithMessage('.product-detail', `<h3>Uh oh! The selected item doesn't exist.</h3>`)
+    }
+  } catch (e) {
     console.log(e);
-        // if the product does not exist do this
-        let htmlEl = document.querySelector(".product-detail");
-        // clear the html where the product would be
-        htmlEl.innerHTML = "";
-        //add an error message
-        const message = `<h3>Uh oh! The selected item doesn't exist.</h3>`;
-        htmlEl.innerHTML = message;
+    // if the product does not exist do this
+    clearHTMLWithMessage('.product-detail', `<h3>Uh oh! The selected item doesn't exist.</h3>`);
   }
 }
 
 function addToCart() {
-    setLocalStorage("so-cart", product);
-    updateCartItemCount();
+  setLocalStorage("so-cart", product);
+  updateCartItemCount();
 
-      // Get the cart icon element
-    const cartIcon = document.querySelector(".cartBackpack");
+  // Get the cart icon element
+  const cartIcon = document.querySelector(".cartBackpack");
 
-    alertMessage("added to cart!")
-    // Add a CSS class to trigger the animation
-    cartIcon.classList.add("cartAnimation");
-    
-    // Remove the animation class after a certain amount of time
-    setTimeout(function() {
+  alertMessage("added to cart!")
+  // Add a CSS class to trigger the animation
+  cartIcon.classList.add("cartAnimation");
+
+  // Remove the animation class after a certain amount of time
+  setTimeout(function () {
     cartIcon.classList.remove("cartAnimation");
-    }, 500);
+  }, 500);
 
 }
 
 function renderProductDetails() {
-    document.querySelector("#productName").innerText = product.Brand.Name;
-    document.querySelector("#productNameWithoutBrand").innerText =
-      product.NameWithoutBrand;
-    document.querySelector("#productImage").src = product.Images.PrimaryLarge;
-    document.querySelector("#productImage").alt = product.Name;
-    document.querySelector("#suggestedPrice").innerText = product.SuggestedRetailPrice;
-    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
-    document.querySelector("#discount").innerText = 100 - Math.round(product.FinalPrice / product.SuggestedRetailPrice * 100);
-    document.querySelector("#productColorName").innerText =
-      product.Colors[0].ColorName;
-    document.querySelector("#productDescriptionHtmlSimple").innerHTML =
-      product.DescriptionHtmlSimple;
-    document.querySelector("#addToCart").dataset.id = product.Id;
-  }
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Images.PrimaryLarge;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#suggestedPrice").innerText = product.SuggestedRetailPrice;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#discount").innerText = 100 - Math.round(product.FinalPrice / product.SuggestedRetailPrice * 100);
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
+}
 
